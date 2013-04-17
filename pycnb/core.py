7073259233bba925b8906a5c4cb4f95f4810e3f0
yac <yac@blesmrt.net>
2013-04-17 20:13:54 +0200

    + -i for interactive prompt with preloaded rates

--- pycnb/core.py
+++ pycnb/core.py
@@ -2,26 +2,47 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-
-import logging
-log = logging.getLogger(__name__)
+from pprint import pprint
+import code
+from decimal import Decimal
 
 from cement.core import foundation, handler, controller
-from pprint import pprint
-from .protocol import get_rates
 from twisted.internet import reactor
+
+from .protocol import get_rates
 
 class MainController(controller.CementBaseController):
     class Meta:
         description = 'PyCNB entry point'
+        arguments = [
+            (['-i', '--interactive'], dict(action='store_true')),
+        ]
+
+    def get_cb(self):
+        if self.pargs.interactive:
+            return self.create_namespace
+        else:
+            return self.print_all
 
     @controller.expose()
     def default(self):
-        d = get_rates(self._gotRates, reactor)
+        d = get_rates(self.get_cb(), reactor)
+
+        if self.pargs.interactive:
+            d.addCallback(self.interact)
+
         d.addBoth(lambda x: reactor.stop())
         reactor.run()
 
-    def _gotRates(self, rates):
+    def create_namespace(self, rates):
+        ns = dict(rates.items())
+        ns['D'] = Decimal
+        return ns
+
+    def interact(self, ns):
+        code.interact(local=ns)
+
+    def print_all(self, rates):
         [print("{0} {1}".format(*i)) for i in rates.items()]
 
 class PyCNBApp(foundation.CementApp):
