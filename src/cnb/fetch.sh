28720e6e82850d732b4d76b3891da976789e74bb
Jan Matějka <jan@matejka.ninja>
2022-07-13 17:44:09 +0200

    wip

--- src/cnb/fetch.sh
+++ src/cnb/fetch.sh
@@ -8,4 +8,7 @@ if [[ -f $CNB_LOCAL_RATES && $(( $(date +%s) - $(date -r $CNB_LOCAL_RATES +%s) )
 fi
 
 mkdir -p $(dirname $CNB_LOCAL_RATES)
+set -x
 curl -s --show-error $CNB_URL -o $CNB_LOCAL_RATES || fatal "curl failed"
+cat ${CNB_URL:7}
+cat $CNB_LOCAL_RATES
